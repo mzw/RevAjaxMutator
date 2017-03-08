6bb322b9c1129e75c9e4c3e5f902be9f8037de9d
Yuta Maezawa <maezawa@nii.ac.jp>
2017-03-08 14:49:20 +0900

    create record dir if not exist

--- src/main/java/jp/mzw/revajaxmutator/RecorderPlugin.java
+++ src/main/java/jp/mzw/revajaxmutator/RecorderPlugin.java
@@ -1,6 +1,7 @@
 package jp.mzw.revajaxmutator;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -11,55 +12,58 @@ import org.owasp.webscarab.model.Response;
 import org.owasp.webscarab.plugin.proxy.ProxyPlugin;
 
 public class RecorderPlugin extends ProxyPlugin {
-    private String mDirname;
+	private String mDirname;
 
-    public RecorderPlugin(String dirname) {
-        mDirname = dirname;
-    }
+	public RecorderPlugin(String dirname) {
+		mDirname = dirname;
+	}
 
-    @Override
-    public String getPluginName() {
-        return "RecorderPlugin";
-    }
+	@Override
+	public String getPluginName() {
+		return "RecorderPlugin";
+	}
 
-    @Override
-    public HTTPClient getProxyPlugin(HTTPClient client) {
-        return new Plugin(client);
-    }
+	@Override
+	public HTTPClient getProxyPlugin(HTTPClient client) {
+		return new Plugin(client);
+	}
 
-    private void recordResponseContent(Request request, Response response) {
-        try {
-            String filename = URLEncoder.encode(request.getURL().toString(), "utf-8");
-            BufferedOutputStream out = new BufferedOutputStream(
-                    new FileOutputStream(mDirname + "/" + filename));
-            out.write(response.getContent());
-            out.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	private void recordResponseContent(Request request, Response response) {
+		try {
+			File dir = new File(mDirname);
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			String filename = URLEncoder.encode(request.getURL().toString(), "utf-8");
+			File file = new File(dir, filename);
+			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+			out.write(response.getContent());
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private class Plugin implements HTTPClient {
-        private HTTPClient mClient;
+	private class Plugin implements HTTPClient {
+		private HTTPClient mClient;
 
-        public Plugin(HTTPClient client) {
-            mClient = client;
-        }
+		public Plugin(HTTPClient client) {
+			mClient = client;
+		}
 
-        public Response fetchResponse(Request request) throws IOException {
-            // remove if-modified-since and if-none-matche to avoid 304
-            request.deleteHeader("If-Modified-Since");
-            request.deleteHeader("If-None-Match");
+		public Response fetchResponse(Request request) throws IOException {
+			// remove if-modified-since and if-none-matche to avoid 304
+			request.deleteHeader("If-Modified-Since");
+			request.deleteHeader("If-None-Match");
 
-            Response response = mClient.fetchResponse(request);
+			Response response = mClient.fetchResponse(request);
 
-            if ("200".equals(response.getStatus())) {
-                recordResponseContent(request, response);
-            }
-            return response;
-        }
+			if ("200".equals(response.getStatus())) {
+				recordResponseContent(request, response);
+			}
+			return response;
+		}
 
-    }
+	}
 
 }
