ce85d8129906eb62faae886adccc7c7f9f7fa5dd
filipe <filipe@filipe.nii>
2017-03-17 16:53:34 +0900

    Added ability for parser to detect EventType repair locations.
    In addition, repairconfig will try automatically to insert similar
    repair keywords into those locations.
    Might increase too much the search space, leading to longer execution
    times.

--- src/main/java/jp/mzw/revajaxmutator/config/mutation/ConfigHelper.java
+++ src/main/java/jp/mzw/revajaxmutator/config/mutation/ConfigHelper.java
@@ -2,13 +2,13 @@ package jp.mzw.revajaxmutator.config.mutation;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import jp.mzw.revajaxmutator.config.app.AppConfig;
-import jp.mzw.revajaxmutator.config.app.DefaultAppConfig;
+import com.google.common.collect.ImmutableSet;
+
 import jp.mzw.revajaxmutator.parser.RepairSource;
 import jp.mzw.revajaxmutator.parser.html.EventSet;
 import jp.mzw.revajaxmutator.parser.html.HTMLParser;
@@ -37,21 +37,21 @@ public class ConfigHelper {
 	}
 
 	public List<RepairSource> getRepairSourcesForDomSelectionAttributeFixer() {
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
-		ArrayList<String> duplicate = new ArrayList<>();
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final ArrayList<String> duplicate = new ArrayList<>();
 
 		// add default
 		repairSources.add(new RepairSource("document", RepairSource.Type.DEFAULT));
 
 		// From Test Case
-		for (String attr : testCaseParser.getAttributeValues()) {
+		for (final String attr : this.testCaseParser.getAttributeValues()) {
 			if (!duplicate.contains(attr)) {
 				duplicate.add(attr);
 				repairSources.add(new RepairSource(attr, RepairSource.Type.TESTCASE));
 			}
 		}
 		// From HTML
-		for (String attr : htmlParser.getAllElementIdentifier()) {
+		for (final String attr : this.htmlParser.getAllElementIdentifier()) {
 			if (!duplicate.contains(attr)) {
 				duplicate.add(attr);
 				repairSources.add(new RepairSource(attr, RepairSource.Type.HTML));
@@ -62,60 +62,88 @@ public class ConfigHelper {
 	}
 
 	public List<RepairSource> getRepairSourcesForEventTarget() {
-		if (eventSet == null) {
-			eventSet = htmlParser.getAllEventSet();
+		if (this.eventSet == null) {
+			this.eventSet = this.htmlParser.getAllEventSet();
 		}
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
-		ArrayList<String> duplicate = new ArrayList<>();
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final ArrayList<String> duplicate = new ArrayList<>();
 
 		// add default
 		repairSources.add(new RepairSource("$(document)", RepairSource.Type.DEFAULT));
 		// From Test Case
-		for (String attr : testCaseParser.getAttributeValues()) {
+		for (final String attr : this.testCaseParser.getAttributeValues()) {
 			if (!duplicate.contains(attr)) {
 				duplicate.add(attr);
 				repairSources.add(new RepairSource("$(" + attr + ")", RepairSource.Type.TESTCASE));
 			}
 		}
 		// From HTML
-		for (String eventTarget : eventSet.getTargetSet()) {
+		for (final String eventTarget : this.eventSet.getTargetSet()) {
 			repairSources.add(new RepairSource(eventTarget, RepairSource.Type.HTML));
 		}
 		return repairSources;
 	}
 
 	public List<RepairSource> getRepairSourcesForEventType() {
-		if (eventSet == null) {
-			eventSet = htmlParser.getAllEventSet();
+		if (this.eventSet == null) {
+			this.eventSet = this.htmlParser.getAllEventSet();
 		}
+
 		// From HTML
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
-		ArrayList<String> duplicate = new ArrayList<>();
-		for (String eventType : eventSet.getTypeSet()) {
-			if (!duplicate.contains(eventType)) {
-				duplicate.add(eventType);
-				repairSources.add(new RepairSource(eventType, RepairSource.Type.HTML));
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final Set<String> htmlSet = new HashSet<>();
+		for (final String eventType : this.eventSet.getTypeSet()) {
+			htmlSet.add(eventType);
+		}
+
+		for (final String eventType : htmlSet) {
+			repairSources.add(new RepairSource(eventType, RepairSource.Type.HTML));
+		}
+
+		// From JavaScript
+		final Set<String> jsSet = new HashSet<>();
+		for (final String name : this.jsParser.getEventTypes()) {
+			// add similar events to the list of repair sources
+			if (keyboardPressETKeywords.contains(name)) {
+				for (final String eventType : keyboardPressETKeywords) {
+					jsSet.add(eventType);
+				}
+			} else if (mouseClickETKeywords.contains(name)) {
+				for (final String eventType : mouseClickETKeywords) {
+					jsSet.add(eventType);
+				}
+			} else if (editETKeywords.contains(name)) {
+				for (final String eventType : editETKeywords) {
+					jsSet.add(eventType);
+				}
 			}
 		}
+
+		// Add new .js event types
+		jsSet.removeAll(htmlSet);
+		for (final String eventType : jsSet) {
+			repairSources.add(new RepairSource("\'" + eventType + "\'", RepairSource.Type.JavaScript));
+		}
+
 		return repairSources;
 	}
 
 	public List<RepairSource> getRepairSourcesForEventCallback() {
-		if (eventSet == null) {
-			eventSet = htmlParser.getAllEventSet();
+		if (this.eventSet == null) {
+			this.eventSet = this.htmlParser.getAllEventSet();
 		}
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
-		ArrayList<String> duplicate = new ArrayList<>();
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final ArrayList<String> duplicate = new ArrayList<>();
 
 		// From HTML
-		for (String eventCallback : eventSet.getCallbackSet()) {
+		for (final String eventCallback : this.eventSet.getCallbackSet()) {
 			if (!duplicate.contains(eventCallback)) {
 				duplicate.add(eventCallback);
 				repairSources.add(new RepairSource(eventCallback, RepairSource.Type.HTML));
 			}
 		}
 		// From JavaScript
-		for (String name : jsParser.getFunctionNames()) {
+		for (final String name : this.jsParser.getFunctionNames()) {
 			if (!duplicate.contains(name)) {
 				duplicate.add(name);
 				repairSources.add(new RepairSource(name, RepairSource.Type.JavaScript));
@@ -125,27 +153,27 @@ public class ConfigHelper {
 	}
 
 	public List<RepairSource> getRepairSourcesForTimerEventDuration() {
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
 		// default
-		String[] durations = new String[] { "0", "50", "100", "250", "500", "750", "1000" };
-		for (String duration : durations) {
+		final String[] durations = new String[] { "0", "50", "100", "250", "500", "750", "1000" };
+		for (final String duration : durations) {
 			repairSources.add(new RepairSource(duration, RepairSource.Type.DEFAULT));
 		}
 		return repairSources;
 	}
 
 	public List<RepairSource> getRepairSourcesForAttributeValues() {
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
-		ArrayList<String> duplicate = new ArrayList<>();
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final ArrayList<String> duplicate = new ArrayList<>();
 		// From JavaScript
-		for (String value : jsParser.getAttributeValuesFromInfixExpression()) {
+		for (final String value : this.jsParser.getAttributeValuesFromInfixExpression()) {
 			if (!duplicate.contains(value)) {
 				duplicate.add(value);
 				repairSources.add(new RepairSource(value, RepairSource.Type.JavaScript));
 			}
 		}
 		// From HTML
-		for (String value : htmlParser.getAllAttributeValues()) {
+		for (final String value : this.htmlParser.getAllAttributeValues()) {
 			if (!duplicate.contains(value)) {
 				duplicate.add(value);
 				repairSources.add(new RepairSource(value, RepairSource.Type.HTML));
@@ -155,13 +183,12 @@ public class ConfigHelper {
 	}
 
 	public List<RepairSource> getRepairSourcesForAppendedDOM() {
-		List<RepairSource> repairSources = new ArrayList<RepairSource>();
+		final List<RepairSource> repairSources = new ArrayList<RepairSource>();
 		return repairSources;
 	}
 
-	public static File getRecordedJsFile(String filename) throws IOException, MalformedURLException, UnsupportedEncodingException {
-		AppConfig config = new DefaultAppConfig(filename);
-		return config.getRecordedJsFile();
-	}
-
+	private static final Set<String> keyboardPressETKeywords = ImmutableSet.of("textinput", "keyup", "keypress", "keydown");
+	private static final Set<String> mouseClickETKeywords = ImmutableSet.of("click", "contextmenu", "dblclick", "mousedown", "mouseup", "mouseenter",
+			"mouseleave", "mouseover", "mousewheel", "wheel");
+	private static final Set<String> editETKeywords = ImmutableSet.of("change", "copy", "cut", "paste", "reset", "select", "textinput");
 }
