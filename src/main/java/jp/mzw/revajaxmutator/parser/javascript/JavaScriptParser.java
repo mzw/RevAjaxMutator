8b7c039e4ce47fe04579c0da75acac2a4eb5c347
filipe <filipe@filipe.nii>
2017-03-17 16:53:34 +0900

    Added ability for parser to detect EventType repair locations.
    In addition, repairconfig will try automatically to insert similar
    repair keywords into those locations.
    Might increase too much the search space, leading to longer execution
    times.

--- src/main/java/jp/mzw/revajaxmutator/parser/javascript/JavaScriptParser.java
+++ src/main/java/jp/mzw/revajaxmutator/parser/javascript/JavaScriptParser.java
@@ -15,50 +15,53 @@ import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ast.AstNode;
 import org.mozilla.javascript.ast.AstRoot;
+import org.mozilla.javascript.ast.FunctionCall;
 import org.mozilla.javascript.ast.FunctionNode;
 import org.mozilla.javascript.ast.InfixExpression;
+import org.mozilla.javascript.ast.Name;
 import org.mozilla.javascript.ast.NodeVisitor;
 import org.mozilla.javascript.ast.PropertyGet;
+import org.mozilla.javascript.ast.StringLiteral;
 import org.mozilla.javascript.tools.shell.Global;
 
 import com.google.common.collect.ImmutableSet;
 
 public class JavaScriptParser {
-	
-	private AstRoot ast;
-	
-	public JavaScriptParser(File file) throws IOException {
-		String src = FileUtils.readFileToString(file);
-		InputStream is = JavaScriptParser.class.getClassLoader().getResourceAsStream("env.rhino.1.2.js");
 
-		Global global = new Global(); 
-		Context cx = ContextFactory.getGlobal().enterContext();
+	private final AstRoot ast;
+
+	public JavaScriptParser(File file) throws IOException {
+		final String src = FileUtils.readFileToString(file);
+		final InputStream is = JavaScriptParser.class.getClassLoader().getResourceAsStream("env.rhino.1.2.js");
+
+		final Global global = new Global();
+		final Context cx = ContextFactory.getGlobal().enterContext();
 		global.init(cx);
 
-		Scriptable scope = cx.initStandardObjects(global);
+		final Scriptable scope = cx.initStandardObjects(global);
 		cx.setOptimizationLevel(-1); // bypass 64kb size limit
 		cx.evaluateReader(scope, new InputStreamReader(is), "env.rhino.js", 1, null);
-		
-		CompilerEnvirons ce = new CompilerEnvirons();
+
+		final CompilerEnvirons ce = new CompilerEnvirons();
 		ce.initFromContext(cx);
-		org.mozilla.javascript.Parser parser = new org.mozilla.javascript.Parser(ce);
-		
-		ast = parser.parse(src, "", 1);
+		final org.mozilla.javascript.Parser parser = new org.mozilla.javascript.Parser(ce);
+
+		this.ast = parser.parse(src, "", 1);
 	}
-	
+
 	public AstRoot getAstRoot() {
 		return this.ast;
 	}
-	
+
 	public List<String> getFunctionNames() {
 		final ArrayList<String> ret = new ArrayList<>();
-		ast.visitAll(new NodeVisitor() {
+		this.ast.visitAll(new NodeVisitor() {
 			@Override
 			public boolean visit(AstNode node) {
-				if(node instanceof FunctionNode) {
-					FunctionNode _node = (FunctionNode) node;
-					String name = _node.getName();
-					if(!"".equals(name)) {
+				if (node instanceof FunctionNode) {
+					final FunctionNode _node = (FunctionNode) node;
+					final String name = _node.getName();
+					if (!"".equals(name)) {
 						ret.add(name);
 					}
 				}
@@ -68,56 +71,51 @@ public class JavaScriptParser {
 		return ret;
 	}
 
-	// Same as those implemented in jp.mzw.ajaxmutator.detector.dom.AttributeAssignmentDetector
-    private final Set<String> globalAttributes = ImmutableSet.of("accessKey",
-            "class", "dir", "id", "lang", "style", "tabindex", "title",
-            "contenteditable", "contextmenu", "draggable", "dropzone",
-            "hidden", "spellcheck");
-    private final Set<String> attributes = ImmutableSet.of("abbr",
-            "accept-charset", "accept", "action", "align", "alink", "alt",
-            "archive", "axis", "background", "bgcolor", "border",
-            "cellpadding", "cellspacing", "char", "charoff", "charset",
-            "checked", "cite", "classid", "clear", "code", "codebase",
-            "codetype", "color", "cols", "colspan", "compact", "content",
-            "coords", "data", "datetime", "declare", "defer", "disabled",
-            "enctype", "face", "for", "frame", "frameborder", "headers",
-            "height", "href", "hreflang", "hspace", "http-equiv", "id",
-            "ismap", "label", "language", "link", "longdesc", "marginheight",
-            "marginwidth", "maxlength", "media", "method", "multiple", "name",
-            "nohref", "noresize", "noshade", "nowrap", "object", "profile",
-            "prompt", "readonly", "rel", "rev", "rows", "rowspan", "rules",
-            "scheme", "scope", "scrolling", "selected", "shape", "size",
-            "span", "src", "standby", "start", "summary", "target", "text",
-            "type", "usemap", "valign", "value", "valuetype", "version",
-            "vlink", "vspace", "width");
-    /**
-     * Detect specific attribute values from infix expressions
-     * because {@link jp.mzw.ajaxmutator.detector.dom.AttributeAssignmentDetector} works for only Assignment
-     * @return
-     */
+	// Same as those implemented in
+	// jp.mzw.ajaxmutator.detector.dom.AttributeAssignmentDetector
+	private final Set<String> globalAttributes = ImmutableSet.of("accessKey", "class", "dir", "id", "lang", "style",
+			"tabindex", "title", "contenteditable", "contextmenu", "draggable", "dropzone", "hidden", "spellcheck");
+	private final Set<String> attributes = ImmutableSet.of("abbr", "accept-charset", "accept", "action", "align",
+			"alink", "alt", "archive", "axis", "background", "bgcolor", "border", "cellpadding", "cellspacing", "char",
+			"charoff", "charset", "checked", "cite", "classid", "clear", "code", "codebase", "codetype", "color",
+			"cols", "colspan", "compact", "content", "coords", "data", "datetime", "declare", "defer", "disabled",
+			"enctype", "face", "for", "frame", "frameborder", "headers", "height", "href", "hreflang", "hspace",
+			"http-equiv", "id", "ismap", "label", "language", "link", "longdesc", "marginheight", "marginwidth",
+			"maxlength", "media", "method", "multiple", "name", "nohref", "noresize", "noshade", "nowrap", "object",
+			"profile", "prompt", "readonly", "rel", "rev", "rows", "rowspan", "rules", "scheme", "scope", "scrolling",
+			"selected", "shape", "size", "span", "src", "standby", "start", "summary", "target", "text", "type",
+			"usemap", "valign", "value", "valuetype", "version", "vlink", "vspace", "width");
+
+	/**
+	 * Detect specific attribute values from infix expressions because
+	 * {@link jp.mzw.ajaxmutator.detector.dom.AttributeAssignmentDetector} works
+	 * for only Assignment
+	 *
+	 * @return
+	 */
 	public List<String> getAttributeValuesFromInfixExpression() {
 		final ArrayList<String> ret = new ArrayList<>();
-		ast.visitAll(new NodeVisitor() {
+		this.ast.visitAll(new NodeVisitor() {
 			@Override
 			public boolean visit(AstNode node) {
-				if(node instanceof InfixExpression) {
-					InfixExpression _node = (InfixExpression) node;
+				if (node instanceof InfixExpression) {
+					final InfixExpression _node = (InfixExpression) node;
 					try { // only for node with operator
 						InfixExpression.operatorToString(_node.getOperator());
-						if(_node.getLeft() instanceof PropertyGet) {
-							PropertyGet _left_node = (PropertyGet) _node.getLeft();
-							String value = _node.getRight().toSource();
-							for(String attr : globalAttributes) {
-								if(attr.equals(_left_node.getProperty().toSource())) {
-									if(!ret.contains(value)) {
+						if (_node.getLeft() instanceof PropertyGet) {
+							final PropertyGet _left_node = (PropertyGet) _node.getLeft();
+							final String value = _node.getRight().toSource();
+							for (final String attr : JavaScriptParser.this.globalAttributes) {
+								if (attr.equals(_left_node.getProperty().toSource())) {
+									if (!ret.contains(value)) {
 										ret.add(value);
 										break;
 									}
 								}
 							}
-							for(String attr : attributes) {
-								if(attr.equals(_left_node.getProperty().toSource())) {
-									if(!ret.contains(value)) {
+							for (final String attr : JavaScriptParser.this.attributes) {
+								if (attr.equals(_left_node.getProperty().toSource())) {
+									if (!ret.contains(value)) {
 										System.out.println(attr + ", " + value + ", " + node.toSource());
 										ret.add(value);
 										break;
@@ -125,8 +123,48 @@ public class JavaScriptParser {
 								}
 							}
 						}
-					} catch (IllegalArgumentException e) {
+					} catch (final IllegalArgumentException e) {
 						// NOP
+					}
+				}
+				return true;
+			}
+		});
+		return ret;
+	}
+
+	private static final Set<String> eventHandlerKeywords = ImmutableSet.of("addEventListener", "on", "off", "bind",
+			"unbind", "delegate", "undelegate", "error", "live", "load", "unload", "one", "trigger");
+	// list taken from http://www.quirksmode.org/dom/events/
+	private static final Set<String> eventTypeKeywords = ImmutableSet.of("blue", "change", "click", "dblclick",
+			"contextmenu", "focus", "focusin", "focusout", "hover", "keydown", "keypress", "keyup", "mousedown",
+			"mouseenter", "mouseleave", "mouseremove", "mouseout", "mouseover", "mouseup", "mousewheel", "copy", "cut",
+			"paste", "resize", "scroll", "select", "submit", "unload");
+
+	public List<String> getEventTypes() {
+		final ArrayList<String> ret = new ArrayList<>();
+		this.ast.visitAll(new NodeVisitor() {
+			@Override
+			public boolean visit(AstNode node) {
+				if (node instanceof FunctionCall) {
+					final FunctionCall functionCall = (FunctionCall) node;
+					// Get the name of the function
+					final Name name = ((PropertyGet) functionCall.getTarget()).getProperty();
+					if (name == null) {
+						// it's an anonymous function
+						return true;
+					}
+
+					// Check if it is an event handler
+					if (eventHandlerKeywords.contains(name.getIdentifier())) {
+						for (final AstNode a : functionCall.getArguments()) {
+							if (a instanceof StringLiteral) {
+								final String argText = ((StringLiteral) a).getValue().toLowerCase();
+								if (eventTypeKeywords.contains(argText)) {
+									ret.add(argText);
+								}
+							}
+						}
 					}
 				}
 				return true;
--- src/test/java/jp/mzw/revajaxmutator/parser/JavaScriptParserTest.java
+++ src/test/java/jp/mzw/revajaxmutator/parser/JavaScriptParserTest.java
@@ -1,6 +1,7 @@
 package jp.mzw.revajaxmutator.parser;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -8,37 +9,45 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 
-import jp.mzw.revajaxmutator.parser.javascript.JavaScriptParser;
-
 import org.junit.Test;
+
+import jp.mzw.revajaxmutator.parser.javascript.JavaScriptParser;
 
 public class JavaScriptParserTest {
 
 	@Test
 	public void parse() throws IOException, URISyntaxException {
-		File file = getTestCaseFile("quizzy.js");
-		JavaScriptParser parser = new JavaScriptParser(file);
+		final File file = this.getTestCaseFile("quizzy.js");
+		final JavaScriptParser parser = new JavaScriptParser(file);
 		assertNotNull(parser.getAstRoot());
 	}
-	
+
 	@Test
 	public void getAllFunctions() throws URISyntaxException, IOException {
-		File file = getTestCaseFile("quizzy.js");
-		JavaScriptParser parser = new JavaScriptParser(file);
-		assertEquals(Arrays.asList("startQuiz", "requestNextQuestion", "checkQuestion", "restartQuizzy"), parser.getFunctionNames());
+		final File file = this.getTestCaseFile("quizzy.js");
+		final JavaScriptParser parser = new JavaScriptParser(file);
+		assertEquals(Arrays.asList("startQuiz", "requestNextQuestion", "checkQuestion", "restartQuizzy"),
+				parser.getFunctionNames());
 	}
 
 	@Test
 	public void getAttributeValuesFromInfixExpression() throws URISyntaxException, IOException {
-		File file = getTestCaseFile("jsparser_test.js");
-		JavaScriptParser parser = new JavaScriptParser(file);
+		final File file = this.getTestCaseFile("jsparser_test.js");
+		final JavaScriptParser parser = new JavaScriptParser(file);
 		assertEquals(Arrays.asList("\"myid\""), parser.getAttributeValuesFromInfixExpression());
 	}
 
+	@Test
+	public void getEventTypes() throws URISyntaxException, IOException {
+		final File file = this.getTestCaseFile("jsparser_test2.js");
+		final JavaScriptParser parser = new JavaScriptParser(file);
+		assertEquals(Arrays.asList("click"), parser.getEventTypes());
+	}
+
 	private File getTestCaseFile(String filename) throws URISyntaxException {
-		ClassLoader loader = JavaScriptParserTest.class.getClassLoader();
-		URL res = loader.getResource(filename);
-		String path = res.toURI().getPath();
+		final ClassLoader loader = JavaScriptParserTest.class.getClassLoader();
+		final URL res = loader.getResource(filename);
+		final String path = res.toURI().getPath();
 		return new File(path);
 	}
 }
