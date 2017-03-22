3ab2b3ed7ec4e25bfa9ce5633b8e7facba838129
Yuta Maezawa <maezawa@nii.ac.jp>
2017-03-22 18:05:32 +0900

    remove user-specific script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /Users/peishun/honiden\ ken/develop/RevAjaxMutator
-mvn jar:jar
-cp -f target/RevAjaxMutator-0.1.jar /Users/peishun/Documents/workspace/TestCode/lib/RevAjaxMutator-0.1.jar 
