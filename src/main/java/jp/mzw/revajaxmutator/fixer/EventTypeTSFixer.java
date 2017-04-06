e2be2e7bbbe1bee33eec18d63b35a22fc9d6d65e
Yuta Maezawa <maezawa@nii.ac.jp>
2017-04-06 19:27:48 +0900

    implement formatAccordingTo at EventTypeTSFixer #25

--- src/main/java/jp/mzw/revajaxmutator/fixer/EventTypeTSFixer.java
+++ src/main/java/jp/mzw/revajaxmutator/fixer/EventTypeTSFixer.java
@@ -8,6 +8,7 @@ import jp.mzw.ajaxmutator.mutatable.EventAttachment;
 import jp.mzw.revajaxmutator.parser.RepairSource;
 
 import org.mozilla.javascript.ast.AstNode;
+import org.mozilla.javascript.ast.StringLiteral;
 
 /**
  * 
@@ -16,19 +17,55 @@ import org.mozilla.javascript.ast.AstNode;
  */
 public class EventTypeTSFixer extends AbstractReplacingAmongFixer<EventAttachment> {
 
-    public EventTypeTSFixer(
-			Collection<EventAttachment> mutationTargets,
-			List<RepairSource> repairSources) {
+	public EventTypeTSFixer(Collection<EventAttachment> mutationTargets, List<RepairSource> repairSources) {
 		super(EventAttachment.class, mutationTargets, repairSources);
 	}
-    
-    public EventTypeTSFixer(
-			Collection<EventAttachment> mutationTargets) {
+
+	public EventTypeTSFixer(Collection<EventAttachment> mutationTargets) {
 		super(EventAttachment.class, mutationTargets, new ArrayList<RepairSource>());
 	}
 
 	@Override
-    protected AstNode getFocusedNode(EventAttachment node) {
-        return node.getEvent();
-    }
+	protected AstNode getFocusedNode(EventAttachment node) {
+		return node.getEvent();
+	}
+
+	/**
+	 * jQuery uses event types that do not contain 'on' prefix.
+	 */
+	@Override
+	protected String formatAccordingTo(AstNode mutatingNode, AstNode mutatedNode) {
+		if ((mutatedNode instanceof StringLiteral) && !(mutatingNode instanceof StringLiteral)) {
+			String mutatingEventType = ((StringLiteral) mutatingNode).getValue();
+			String mutatedEventType = mutatedNode.toSource();
+			return '"' + manipulateOnPrefix(mutatingEventType, mutatedEventType) + '"';
+		} else if (!(mutatedNode instanceof StringLiteral) && (mutatingNode instanceof StringLiteral)) {
+			String mutatingEventType = mutatingNode.toSource();
+			String mutatedEventType = ((StringLiteral) mutatedNode).getValue();
+			return manipulateOnPrefix(mutatingEventType, mutatedEventType);
+		} else if ((mutatedNode instanceof StringLiteral) && (mutatingNode instanceof StringLiteral)) {
+			String mutatingEventType = ((StringLiteral) mutatingNode).getValue();
+			String mutatedEventType = ((StringLiteral) mutatedNode).getValue();
+			return '"' + manipulateOnPrefix(mutatingEventType, mutatedEventType) + '"';
+		}
+		return super.formatAccordingTo(mutatingNode, mutatedNode);
+	}
+
+	/**
+	 * Case #1: If mutated start with 'on' but mutating does not start with 'on', return 'on' + mutating.
+	 * Case #2: If mutated does not start with 'on' but mutating starts with 'on', return mutating without 'on'.
+	 * Otherwise, return mutating.
+	 * 
+	 * @param mutatingEventType candidate
+	 * @param mutatedEventType base
+	 * @return proper event type
+	 */
+	private String manipulateOnPrefix(String mutatingEventType, String mutatedEventType) {
+		if (mutatingEventType.startsWith("on") && !mutatedEventType.startsWith("on")) {
+			return mutatingEventType.substring(2);
+		} else if (!mutatingEventType.startsWith("on") && mutatedEventType.startsWith("on")) {
+			return "on" + mutatingEventType;
+		}
+		return mutatingEventType;
+	}
 }
