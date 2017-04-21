c8cf0855b7e9743d5706585033c4f6f61430edae
jopopyv <chihiro.iida7@gmail.com>
2017-04-21 15:01:45 +0900

    implemented DOMCloningToNoOpFixerTest.java, DOMCreationToNoOpFixerTest.java, DOMNormalizationToNoOpFixerTest.java, DOMRemovalToNoOpFixerTest.java and DOMReplacementSrcTargetFixerTest.java.

---
+++ src/test/java/jp/mzw/revajaxmutator/fixer/DOMNormalizationToNoOpFixerTest.java
@@ -0,0 +1,41 @@
+package jp.mzw.revajaxmutator.fixer;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+
+import jp.mzw.ajaxmutator.MutateVisitor;
+import jp.mzw.ajaxmutator.MutateVisitorBuilder;
+import jp.mzw.ajaxmutator.detector.dom.DOMNormalizationDetector;
+import jp.mzw.ajaxmutator.mutatable.DOMNormalization;
+import jp.mzw.ajaxmutator.generator.Mutation;
+import jp.mzw.ajaxmutator.mutator.Mutator;
+import jp.mzw.ajaxmutator.mutator.MutatorTestBase;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class DOMNormalizationToNoOpFixerTest extends MutatorTestBase{
+	@Override
+    protected void prepare() {
+        inputs = new String[] {
+                "document.findElementById('foo').normalize();",
+        };
+
+        MutateVisitorBuilder builder = MutateVisitor.emptyBuilder();
+        builder.setDomNormalizationDetectors(ImmutableSet.of(new DOMNormalizationDetector()));
+        visitor = builder.build();
+    }
+
+	@Test
+	public void testDOMCloningToNoOpFixer() {
+		Set<DOMNormalization> normalizations = visitor.getDomNormalizations();
+		Mutator<DOMNormalization> mutator = new DOMNormalizationToNoOpFixer();
+		List<Mutation> mutationList = mutator.generateMutationList(Iterables.get(normalizations, 0));
+		assertEquals(inputs[0], mutationList.get(0).getOriginalNode().toSource().trim());
+		assertEquals(ReplacingToNoOpFixer.NO_OPERATION_STR, mutationList.get(0).getMutatingContent());
+	}
+}
---
+++ src/test/java/jp/mzw/revajaxmutator/fixer/DOMReplacementSrcTargetFixerTest.java
@@ -0,0 +1,51 @@
+package jp.mzw.revajaxmutator.fixer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+
+import jp.mzw.ajaxmutator.MutateVisitor;
+import jp.mzw.ajaxmutator.MutateVisitorBuilder;
+import jp.mzw.ajaxmutator.detector.dom.ReplaceChildDetector;
+import jp.mzw.ajaxmutator.detector.jquery.JQueryReplaceWithDetector;
+import jp.mzw.ajaxmutator.generator.Mutation;
+import jp.mzw.ajaxmutator.mutatable.DOMReplacement;
+import jp.mzw.ajaxmutator.mutator.Mutator;
+import jp.mzw.ajaxmutator.mutator.MutatorTestBase;
+
+public class DOMReplacementSrcTargetFixerTest extends MutatorTestBase {
+	@Override
+	protected void prepare() {
+		inputs = new String[] {
+				"document.findElementById('hoge').replaceChild(child1, child2);", 
+				"$foo.replaceWith($('#bar'));"
+		};
+		
+		MutateVisitorBuilder builder = MutateVisitor.emptyBuilder();
+		builder.setDomReplacementDetectors(
+				ImmutableSet.of(new ReplaceChildDetector(), new JQueryReplaceWithDetector()));
+		visitor = builder.build();
+	}
+	
+	@Test
+	public void testDOMRemovalToNoOpFixer() {
+		Set<DOMReplacement> domReplacements = visitor.getDomReplacements();
+		Mutator<DOMReplacement> mutator = new DOMReplacementSrcTargetFixer();
+		List<Mutation> mutationList = mutator.generateMutationList(Iterables.get(domReplacements, 0));
+        assertEquals(
+                "document.findElementById('hoge').replaceChild(child1, child2)",
+                mutationList.get(0).getOriginalNode().toSource());
+        assertEquals(
+                "document.findElementById('hoge').replaceChild(child2, child1)",
+                mutationList.get(0).getMutatingContent());
+        mutationList = mutator.generateMutationList(Iterables.get(domReplacements, 1));
+        assertEquals("$foo.replaceWith($('#bar'))", mutationList.get(0).getOriginalNode().toSource());
+        assertEquals("$('#bar').replaceWith($foo)", mutationList.get(0).getMutatingContent());
+	}
+}
