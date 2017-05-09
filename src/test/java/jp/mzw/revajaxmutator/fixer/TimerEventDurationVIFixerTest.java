b61da591269a13c38453d349ba86d29402cb715e
iida <chihiro.iida7@gmail.com>
2017-05-09 21:34:15 +0900

    modified TimerEventDurationVIFixerTest.java

--- src/test/java/jp/mzw/revajaxmutator/fixer/TimerEventDurationVIFixerTest.java
+++ src/test/java/jp/mzw/revajaxmutator/fixer/TimerEventDurationVIFixerTest.java
@@ -1,7 +1,9 @@
 package jp.mzw.revajaxmutator.fixer;
 
 import static org.junit.Assert.assertEquals;
+import static jp.mzw.ajaxmutator.util.StringToAst.parseAstRoot;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -21,6 +23,7 @@ import jp.mzw.ajaxmutator.mutator.Mutator;
 import jp.mzw.ajaxmutator.mutator.MutatorTestBase;
 import jp.mzw.revajaxmutator.config.mutation.ConfigHelper;
 import jp.mzw.revajaxmutator.parser.RepairSource;
+import org.mozilla.javascript.ast.AstRoot;
 
 public class TimerEventDurationVIFixerTest extends MutatorTestBase {
 	private String[] callbacks;
@@ -29,12 +32,12 @@ public class TimerEventDurationVIFixerTest extends MutatorTestBase {
 	
 	private static ConfigHelper helper = new ConfigHelper();
 	
-	private Collection<? extends RepairSource> repairSources;
+	private List<RepairSource> repairSources;
 	
 	@Override
 	public void prepare() {
 		callbacks = new String[] {"func1", "func2"};
-		durations = new String[] {"250", "250"};
+		durations = new String[] {"300", "duration"};
 		inputs = new String[2];
 		for (int i = 0; i < 2; i++) {
 			inputs[i] = (i == 1 ? "window." : "") + setTimeout(callbacks[i], durations[i], i == 0);
@@ -45,7 +48,10 @@ public class TimerEventDurationVIFixerTest extends MutatorTestBase {
 		builder.setTimerEventDetectors(attacherDetector);
 		visitor = builder.build();
 		timerEventAttachments = visitor.getTimerEventAttachmentExpressions();
-		repairSources = helper.getRepairSourcesForTimerEventDuration();
+		repairSources = new ArrayList<RepairSource>();
+		for (int i = 0; i < 2; i++) {
+			repairSources.add(new RepairSource(durations[i], RepairSource.Type.Default));
+		}
 	}
 	
 	@Test
@@ -53,7 +59,7 @@ public class TimerEventDurationVIFixerTest extends MutatorTestBase {
 		Mutator<TimerEventAttachment> fixer = new TimerEventDurationVIFixer(repairSources);
 		List<Mutation> mutationList;
 		mutationList = fixer.generateMutationList(Iterables.get(timerEventAttachments, 0));
-		assertEquals(durations[1], mutationList.get(0).getMutatingContent());
+		assertEquals(durations[0], mutationList.get(0).getMutatingContent());
 		mutationList = fixer.generateMutationList(Iterables.get(timerEventAttachments, 1));
 		assertEquals(durations[0], mutationList.get(0).getMutatingContent());
 	}
