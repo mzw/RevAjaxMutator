c1b1d47f88d821806aafaa85d7d818b4ebf9c1f7
Yuta Maezawa <maezawa@nii.ac.jp>
2017-06-01 15:48:23 +0900

    remove WebScarab conversations after running tests

--- src/main/java/jp/mzw/revajaxmutator/proxy/ProxyServer.java
+++ src/main/java/jp/mzw/revajaxmutator/proxy/ProxyServer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.owasp.webscarab.model.Preferences;
 import org.owasp.webscarab.model.StoreException;
 import org.owasp.webscarab.plugin.Framework;
@@ -13,11 +14,12 @@ import org.owasp.webscarab.plugin.proxy.ProxyPlugin;
 
 public class ProxyServer {
 	protected static Proxy mProxy;
+	protected static final File conversationDir = new File(".conversation");
 
 	public static void launch(List<ProxyPlugin> plugins, String proxy) throws StoreException, InterruptedException {
 		final Framework framework = new Framework();
 		Preferences.setPreference("Proxy.listeners", proxy);
-		framework.setSession("FileSystem", new File(".conversation"), "");
+		framework.setSession("FileSystem", conversationDir, "");
 		mProxy = new Proxy(framework);
 		if (plugins != null) {
 			for (final ProxyPlugin plugin : plugins) {
@@ -48,6 +50,12 @@ public class ProxyServer {
 	public static void interrupt() {
 		if (mProxy != null) {
 			mProxy.stop();
+		}
+	}
+
+	public static void removeConversationDir() throws IOException {
+		if (conversationDir.exists()) {
+			FileUtils.forceDeleteOnExit(conversationDir);
 		}
 	}
 
